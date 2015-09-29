90d574f67932d646e02f6535819d03b172ebc766
koekeishiya <aasvi93@hotmail.com>
2015-09-29 15:05:57 +0200

    can now enable/disable keytap, as well as define custom or system wide hotkeys

--- ffmouse.cpp
+++ ffmouse.cpp
@@ -4,11 +4,159 @@ static const std::string OSASCRIPT_START = "osascript -e 'tell application \"Sys
 static const std::string OSASCRIPT_MID = "\" to perform action \"AXRaise\" of (first window whose name contains \"";
 static const std::string OSASCRIPT_END = "\")'";
 
+static const CGKeyCode kVK_SPECIAL_Å = 0x21;
+static const CGKeyCode kVK_SPECIAL_Ø = 0x29;
+static const CGKeyCode kVK_SPECIAL_Æ = 0x27;
+
 static pid_t pid;
 static ProcessSerialNumber psn;
 static std::vector<app_info> window_lst;
 static int window_lst_focus_index;
 static std::string window_lst_focus_name = "<no focus>";;
+static bool toggle_tap = true;
+
+bool toggle_tap_hotkey(bool cmd_key, bool ctrl_key, bool alt_key, CGKeyCode keycode)
+{
+    if(cmd_key && alt_key && ctrl_key)
+    {
+        if(keycode == kVK_ANSI_T)
+        {
+            toggle_tap = !toggle_tap;
+            std::cout << (toggle_tap ? "tap enabled" : "tap disabled") << std::endl;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool system_hotkey_passthrough(bool cmd_key, bool ctrl_key, bool alt_key, CGKeyCode keycode)
+{
+    // Spotlight fix
+    if (cmd_key && !ctrl_key && !alt_key)
+    {
+        if (keycode == kVK_Space)
+        {
+            toggle_tap = false;
+            std::cout << "tap disabled" << std::endl;
+            return true;
+        }
+    }
+
+    if(cmd_key && ctrl_key && !alt_key)
+    {
+        // Hammerspoon hotkeys -> window movement
+        if(keycode == kVK_ANSI_P
+            || (keycode == kVK_ANSI_N)
+            || (keycode == kVK_ANSI_H)
+            || (keycode == kVK_ANSI_J)
+            || (keycode == kVK_ANSI_K)
+            || (keycode == kVK_ANSI_L))
+            return true;
+    }
+
+    if(cmd_key && alt_key && ctrl_key)
+    {
+        // Hammerspoon hotkeys -> config reload
+        if(keycode == kVK_ANSI_R)
+            return true;
+
+        // Hammerspoon hotkeys -> window resize
+        if(keycode == kVK_LeftArrow
+            || (keycode == kVK_UpArrow)
+            || (keycode == kVK_DownArrow)
+            || (keycode == kVK_RightArrow)
+            || (keycode == kVK_ANSI_M)
+            || (keycode == kVK_ANSI_H)
+            || (keycode == kVK_ANSI_J)
+            || (keycode == kVK_ANSI_K)
+            || (keycode == kVK_ANSI_L)
+            || (keycode == kVK_ANSI_P)
+            || (keycode == kVK_SPECIAL_Å)
+            || (keycode == kVK_SPECIAL_Ø)
+            || (keycode == kVK_SPECIAL_Æ))
+            return true;
+
+        // Hammerspoon hotkeys -> launch app
+        if(keycode == kVK_ANSI_1)
+            return true;
+    }
+    return false;
+}
+
+bool custom_hotkey_commands(bool cmd_key, bool ctrl_key, bool alt_key, CGKeyCode keycode)
+{
+    if(cmd_key && alt_key && ctrl_key)
+    {
+        // YTD - Media Player Controls
+        std::string sys_command = "";
+        if(keycode == kVK_ANSI_Z)
+            sys_command = "ytc prev";
+        else if(keycode == kVK_ANSI_X)
+            sys_command = "ytc play";
+        else if(keycode == kVK_ANSI_C)
+            sys_command = "ytc next";
+        else if(keycode == kVK_ANSI_V)
+            sys_command = "ytc stop";
+
+        if(sys_command != "")
+        {
+            system(sys_command.c_str());
+            return true;
+        }
+    }
+    return false;
+}
+
+CGEventRef cgevent_callback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon)
+{
+    if(type != kCGEventKeyDown && type != kCGEventKeyUp && type != kCGEventMouseMoved)
+        return event;
+
+    if(type == kCGEventKeyDown)
+    {
+        CGEventFlags flags = CGEventGetFlags(event);
+        bool cmd_key = (flags & kCGEventFlagMaskCommand) == kCGEventFlagMaskCommand;
+        bool alt_key = (flags & kCGEventFlagMaskAlternate) == kCGEventFlagMaskAlternate;
+        bool ctrl_key = (flags & kCGEventFlagMaskControl) == kCGEventFlagMaskControl;
+        CGKeyCode keycode = (CGKeyCode)CGEventGetIntegerValueField(event, kCGKeyboardEventKeycode);
+
+        // Toggle keytap on | off
+        if(toggle_tap_hotkey(cmd_key, ctrl_key, alt_key, keycode))
+            return NULL;
+
+        if(toggle_tap)
+        {
+            // Let system hotkeys pass through as normal
+            if(system_hotkey_passthrough(cmd_key, ctrl_key, alt_key, keycode))
+                return event;
+            
+            // capture custom hotkeys
+            if(custom_hotkey_commands(cmd_key, ctrl_key, alt_key, keycode))
+                return NULL;
+        }
+        else
+        {
+            // Re-enable keytap when spotlight is closed
+            if ((cmd_key && keycode == kVK_Space)
+                    || keycode == kVK_Return || keycode == kVK_ANSI_KeypadEnter)
+            {
+                toggle_tap = true;
+                std::cout << "tap enabled" << std::endl;
+            }
+            return event;
+        }
+
+        CGEventSetIntegerValueField(event, kCGKeyboardEventAutorepeat, 0);
+        CGEventPostToPSN(&psn, event);
+        return NULL;
+    }
+    else if(type == kCGEventKeyUp)
+        return event;
+    else if(type == kCGEventMouseMoved)
+        detect_window_below_cursor();
+
+    return event;
+}
 
 void detect_window_below_cursor()
 {
@@ -29,7 +177,7 @@ void detect_window_below_cursor()
         CGPoint cursor = CGEventGetLocation(event);
         CFRelease(event);
         
-        std::cout << "Mouse Pos: " << cursor.x << ", " << cursor.y << std::endl;
+        //std::cout << "Mouse Pos: " << cursor.x << ", " << cursor.y << std::endl;
         for(int i = 0; i < window_lst.size(); ++i)
         {
             if(window_lst[i].layer == 0)
@@ -45,19 +193,23 @@ void detect_window_below_cursor()
                         // Strip single quotes and remaining part if found,
                         // because  it breaks applescript syntax
                         std::string win_title = window_lst[window_lst_focus_index].name;
-                        std::size_t pos = win_title.find("'");
-                        if(pos != std::string::npos)
-                            win_title.erase(pos);
+                        if(win_title != "")
+                        {
+                            std::size_t pos = win_title.find("'");
+                            if(pos != std::string::npos)
+                                win_title.erase(pos);
 
-                        std::string applescript_cmd = OSASCRIPT_START +
-                            window_lst[window_lst_focus_index].owner + OSASCRIPT_MID +
-                            win_title + OSASCRIPT_END;
-                        system(applescript_cmd.c_str());
+                            std::string applescript_cmd = OSASCRIPT_START +
+                                window_lst[window_lst_focus_index].owner + OSASCRIPT_MID +
+                                win_title + OSASCRIPT_END;
+                            system(applescript_cmd.c_str());
+                        }
 
                         if(pid != window_lst[i].pid)
                         {
                             pid = window_lst[i].pid;
                             GetProcessForPID(pid, &psn);
+                            std::cout << "Keyboard focus: " << pid << std::endl;
                         }
                     }
                     break;
@@ -75,43 +227,7 @@ void detect_window_below_cursor()
                 */
             }
         }
-        std::cout << "Keyboard focus: " << pid << std::endl;
     }
-}
-
-CGEventRef cgevent_callback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon)
-{
-    if(type != kCGEventKeyDown && type != kCGEventKeyUp && type != kCGEventMouseMoved)
-        return event;
-
-    if(type == kCGEventKeyDown)
-    {
-        CGEventFlags flags = CGEventGetFlags(event);
-        bool command_key = (flags & kCGEventFlagMaskCommand) == kCGEventFlagMaskCommand;
-        bool alt_key = (flags & kCGEventFlagMaskAlternate) == kCGEventFlagMaskAlternate;
-        bool ctrl_key = (flags & kCGEventFlagMaskControl) == kCGEventFlagMaskControl;
-        if(command_key && alt_key && ctrl_key)
-        {
-            // Check for specific combinations to perform window tiling
-            /*
-            CGKeyCode keycode = (CGKeyCode)CGEventGetIntegerValueField(event, kCGKeyboardEventKeycode);
-            if(keycode == (CGKeyCode)0)
-            {
-            }
-            */
-            return event;
-        }
-
-        CGEventSetIntegerValueField(event, kCGKeyboardEventAutorepeat, 0);
-        CGEventPostToPSN(&psn, event);
-        return NULL;
-    }
-    else if(type == kCGEventKeyUp)
-        return NULL;
-    else if(type == kCGEventMouseMoved)
-        detect_window_below_cursor();
-
-    return event;
 }
 
 void get_window_info(const void *key, const void *value, void *context)
