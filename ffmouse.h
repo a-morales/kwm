030f023b886bd0925c7cef05a643ec2e464a4b2a
koekeishiya <aasvi93@hotmail.com>
2015-09-29 15:06:18 +0200

    function prototypes

--- ffmouse.h
+++ ffmouse.h
@@ -21,9 +21,12 @@ struct app_info
     int width, height;
 };
 
+bool toggle_tap_hotkey(bool cmd_key, bool ctrl_key, bool alt_key, CGKeyCode keycode);
+bool system_hotkey_passthrough(bool cmd_key, bool ctrl_key, bool alt_key, CGKeyCode keycode);
+bool custom_hotkey_commands(bool cmd_key, bool ctrl_key, bool alt_key, CGKeyCode keycode);
 CGEventRef cgevent_callback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon);
-void get_window_info(const void *key, const void *value, void *context);
 void detect_window_below_cursor();
+void get_window_info(const void *key, const void *value, void *context);
 void fatal(const std::string &err);
 
 #endif
