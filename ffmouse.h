e787e360f925722ee2a2f7b5e4db2754cfe046c1
koekeishiya <aasvi93@hotmail.com>
2015-10-02 01:17:22 +0200

    merged send window to prev/next screen

--- ffmouse.h
+++ ffmouse.h
@@ -63,33 +63,30 @@ screen_info *get_display_of_window(window_info *window);
 std::vector<window_info*> get_all_windows_on_display(int screen_index);
 
 void apply_layout_for_screen(int screen_index);
-void cycle_focused_window(int screen_index, int shift);
-
-bool get_window_ref(window_info *window, AXUIElementRef *window_ref);
-bool windows_are_equal(window_info *window, window_info *match);
+void cycle_focused_window_layout(int screen_index, int shift);
+void cycle_focused_window_screen(int shift);
 
 void init_window_layouts();
 void set_window_layout_values(window_layout *layout, int x, int y, int width, int height);
 window_layout get_window_layout_for_screen(int screen_index, const std::string &name);
-bool window_has_layout(window_info *window, window_layout *layout);
-window_layout *get_layout_of_window(window_info *window);
 
 void set_window_dimensions(AXUIElementRef app_window, window_info *window, int x, int y, int width, int height);
-
-void send_window_to_prev_screen();
-void send_window_to_next_screen();
+window_layout *get_layout_of_window(window_info *window);
+bool window_has_layout(window_info *window, window_layout *layout);
 
 bool ffmouse_hotkey_commands(bool cmd_key, bool ctrl_key, bool alt_key, CGKeyCode keycode);
 bool system_hotkey_passthrough(bool cmd_key, bool ctrl_key, bool alt_key, CGKeyCode keycode);
 bool custom_hotkey_commands(bool cmd_key, bool ctrl_key, bool alt_key, CGKeyCode keycode);
 
-void detect_window_below_cursor();
 bool is_window_below_cursor(window_info *window);
+void detect_window_below_cursor();
 
-window_info get_window_info_from_ref(AXUIElementRef window_ref);
+bool get_window_ref(window_info *window, AXUIElementRef *window_ref);
 void get_window_info(const void *key, const void *value, void *context);
-CGEventRef cgevent_callback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon);
+window_info get_window_info_from_ref(AXUIElementRef window_ref);
+bool windows_are_equal(window_info *window, window_info *match);
 
+CGEventRef cgevent_callback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon);
 void fatal(const std::string &err);
 
 #endif
