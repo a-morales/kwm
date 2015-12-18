05faacbb96624192824bbbbf1a57f6acd96fb792
koekeishiya <aasvi93@hotmail.com>
2015-12-18 21:15:21 +0100

    link framework cocoa

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 g++ hotkeys.cpp -DDEBUG_BUILD -shared -O3 -o hotkeys.so
-g++ kwm.cpp tree.cpp window.cpp display.cpp daemon.cpp -DDEBUG_BUILD -O3 -o kwm -lpthread -framework ApplicationServices -framework Carbon
+g++ kwm.cpp tree.cpp window.cpp display.cpp daemon.cpp -DDEBUG_BUILD -O3 -o kwm -lpthread -framework ApplicationServices -framework Carbon -framework Cocoa
