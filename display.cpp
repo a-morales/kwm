a-morales/kwm

commit 60a849b50aeb0f9539f7c852b521c43f39886edc

should no longer segfault when switching monitor if it was connected after kwm started

window.cpp
@@ -286,6 +286,11 @@ void UpdateActiveWindowList(screen_info *Screen)
                     usleep(200000);
                 } while(PrevSpace == CurrentSpace);
                 Screen->ActiveSpace = CurrentSpace;
+
+                if(DisplayIdentifier)
+                    CFRelease(DisplayIdentifier);
+
+                DisplayIdentifier = CGSCopyManagedDisplayForSpace(CGSDefaultConnection, CurrentSpace);
             }
             else
             {
@@ -293,10 +298,6 @@ void UpdateActiveWindowList(screen_info *Screen)
             }
 
             DEBUG("UpdateActiveWindowList() Active Display Changed")
-            if(DisplayIdentifier)
-                CFRelease(DisplayIdentifier);
-
-            DisplayIdentifier = CGSCopyManagedDisplayForSpace(CGSDefaultConnection, CurrentSpace);
             FocusWindowBelowCursor();
         }
         else

commit 139717cb0937b35d194a273e6092c2dee275e881

fixed issue caused by changes in commit 60a849b

display.cpp
@@ -48,7 +48,7 @@ screen_info CreateDefaultScreenInfo(int DisplayIndex, int ScreenIndex)
 
     Screen.ID = ScreenIndex;
     Screen.ForceContainerUpdate = false;
-    Screen.ActiveSpace = -1;
+    Screen.ActiveSpace = 0;
     Screen.OldWindowListCount = -1;
 
     Screen.X = DisplayRect.origin.x;

window.cpp
@@ -278,7 +278,7 @@ void UpdateActiveWindowList(screen_info *Screen)
         PrevSpace = CurrentSpace;
         if(OldScreenID != Screen->ID)
         {
-            if(Screen->ActiveSpace == -1)
+            if(Screen->ActiveSpace == 0)
             {
                 do
                 {
@@ -286,16 +286,16 @@ void UpdateActiveWindowList(screen_info *Screen)
                     usleep(200000);
                 } while(PrevSpace == CurrentSpace);
                 Screen->ActiveSpace = CurrentSpace;
-
-                if(DisplayIdentifier)
-                    CFRelease(DisplayIdentifier);
-
-                DisplayIdentifier = CGSCopyManagedDisplayForSpace(CGSDefaultConnection, Screen->ActiveSpace);
             }
             else
             {
                 CurrentSpace = Screen->ActiveSpace;
             }
+
+            if(DisplayIdentifier)
+                CFRelease(DisplayIdentifier);
+
+            DisplayIdentifier = CGSCopyManagedDisplayForSpace(CGSDefaultConnection, Screen->ActiveSpace);
 
             if(Screen->ForceContainerUpdate)
             {