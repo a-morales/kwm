827919050d8f67ca2cde9eb089fb6686b4605fdb
koekeishiya <aasvi93@hotmail.com>
2016-01-23 19:31:37 +0100

    use CGGetOnlineDisplayList instead of Active

--- kwm/display.cpp
+++ kwm/display.cpp
@@ -70,7 +70,8 @@ void UpdateExistingScreenInfo(screen_info *Screen, int DisplayIndex, int ScreenI
 void GetActiveDisplays()
 {
     KWMScreen.Displays = (CGDirectDisplayID*) malloc(sizeof(CGDirectDisplayID) * KWMScreen.MaxCount);
-    CGGetActiveDisplayList(KWMScreen.MaxCount, KWMScreen.Displays, &KWMScreen.ActiveCount);
+    CGGetOnlineDisplayList(KWMScreen.MaxCount, KWMScreen.Displays, &KWMScreen.ActiveCount);
+    //CGGetActiveDisplayList(KWMScreen.MaxCount, KWMScreen.Displays, &KWMScreen.ActiveCount);
     for(std::size_t DisplayIndex = 0; DisplayIndex < KWMScreen.ActiveCount; ++DisplayIndex)
     {
         unsigned int DisplayID = KWMScreen.Displays[DisplayIndex];
@@ -89,7 +90,8 @@ void RefreshActiveDisplays()
         free(KWMScreen.Displays);
 
     KWMScreen.Displays = (CGDirectDisplayID*) malloc(sizeof(CGDirectDisplayID) * KWMScreen.MaxCount);
-    CGGetActiveDisplayList(KWMScreen.MaxCount, KWMScreen.Displays, &KWMScreen.ActiveCount);
+    CGGetOnlineDisplayList(KWMScreen.MaxCount, KWMScreen.Displays, &KWMScreen.ActiveCount);
+    //CGGetActiveDisplayList(KWMScreen.MaxCount, KWMScreen.Displays, &KWMScreen.ActiveCount);
     for(std::size_t DisplayIndex = 0; DisplayIndex < KWMScreen.ActiveCount; ++DisplayIndex)
     {
         unsigned int DisplayID = KWMScreen.Displays[DisplayIndex];
