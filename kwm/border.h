77a2b2d1884b7e75a93b8dabe8bf64c177cf9e21
koekeishiya <aasvi93@hotmail.com>
2016-02-07 21:37:48 +0100

    missing ; in function declaration

--- kwm/border.h
+++ kwm/border.h
@@ -6,7 +6,7 @@
 std::string ConvertHexToRGBAString(int WindowID, int Color, int Width);
 kwm_time_point PerformUpdateBorderTimer(kwm_time_point Time);
 
-void OpenBorder(kwm_border *Border)
+void OpenBorder(kwm_border *Border);
 void RefreshBorder(kwm_border *Border, int WindowID);
 void ClearBorder(kwm_border *Border);
 void CloseBorder(kwm_border *Border);
