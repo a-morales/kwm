2936949aec3b0193f73a5b3942891ef94a68784c
koekeishiya <aasvi93@hotmail.com>
2016-02-18 10:45:07 +0100

    version 1.1.2

--- kwm/kwm.cpp
+++ kwm/kwm.cpp
@@ -8,7 +8,7 @@
 #include "interpreter.h"
 #include "border.h"
 
-const std::string KwmCurrentVersion = "Kwm Version 1.1.1";
+const std::string KwmCurrentVersion = "Kwm Version 1.1.2";
 
 kwm_mach KWMMach = {};
 kwm_path KWMPath = {};
