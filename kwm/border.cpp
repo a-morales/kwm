0d9a6f259409d725a82cbc2b467417f5af256d3f
koekeishiya <aasvi93@hotmail.com>
2016-02-29 19:59:51 +0100

    inline functions

--- kwm/border.cpp
+++ kwm/border.cpp
@@ -1,59 +1,6 @@
 #include "border.h"
 #include "window.h"
 
-extern kwm_screen KWMScreen;
-extern kwm_focus KWMFocus;
-extern kwm_path KWMPath;
-extern kwm_border FocusedBorder;
-extern kwm_border MarkedBorder;
-extern kwm_border PrefixBorder;
-extern kwm_hotkeys KWMHotkeys;
-
-void ClearBorder(kwm_border *Border)
-{
-    if(Border->Handle)
-    {
-        static std::string Command = "clear";
-        fwrite(Command.c_str(), Command.size(), 1, Border->Handle);
-        fflush(Border->Handle);
-    }
-}
-
-void OpenBorder(kwm_border *Border)
-{
-    if(Border->Enabled && !Border->Handle)
-    {
-        static std::string OverlayBin = KWMPath.FilePath + "/kwm-overlay";
-        Border->Handle = popen(OverlayBin.c_str(), "w");
-        if(!Border->Handle)
-            Border->Enabled = false;
-    }
-}
-
-void RefreshBorder(kwm_border *Border, int WindowID)
-{
-    std::string Command = std::to_string(WindowID) + \
-                          " " + Border->Color.Format + \
-                          " s:" + std::to_string(Border->Width);
-
-    Command += Border->Radius != -1 ? " rad:" + std::to_string(Border->Radius) : "";
-
-    fwrite(Command.c_str(), Command.size(), 1, Border->Handle);
-    fflush(Border->Handle);
-}
-
-void CloseBorder(kwm_border *Border)
-{
-    if(Border->Handle)
-    {
-        static std::string Terminate = "quit";
-        fwrite(Terminate.c_str(), Terminate.size(), 1, Border->Handle);
-        fflush(Border->Handle);
-        pclose(Border->Handle);
-        Border->Handle = NULL;
-    }
-}
-
 void UpdateBorder(std::string BorderType)
 {
     Assert(BorderType == "focused" ||
--- kwm/border.h
+++ kwm/border.h
@@ -3,10 +3,62 @@
 
 #include "types.h"
 
-void OpenBorder(kwm_border *Border);
-void RefreshBorder(kwm_border *Border, int WindowID);
-void ClearBorder(kwm_border *Border);
-void CloseBorder(kwm_border *Border);
+extern kwm_screen KWMScreen;
+extern kwm_focus KWMFocus;
+extern kwm_path KWMPath;
+extern kwm_border FocusedBorder;
+extern kwm_border MarkedBorder;
+extern kwm_border PrefixBorder;
+extern kwm_hotkeys KWMHotkeys;
+
+inline void
+OpenBorder(kwm_border *Border)
+{
+    if(Border->Enabled && !Border->Handle)
+    {
+        static std::string OverlayBin = KWMPath.FilePath + "/kwm-overlay";
+        Border->Handle = popen(OverlayBin.c_str(), "w");
+        if(!Border->Handle)
+            Border->Enabled = false;
+    }
+}
+
+inline void
+CloseBorder(kwm_border *Border)
+{
+    if(Border->Handle)
+    {
+        static std::string Terminate = "quit";
+        fwrite(Terminate.c_str(), Terminate.size(), 1, Border->Handle);
+        fflush(Border->Handle);
+        pclose(Border->Handle);
+        Border->Handle = NULL;
+    }
+}
+
+inline void
+ClearBorder(kwm_border *Border)
+{
+    if(Border->Handle)
+    {
+        static std::string Command = "clear";
+        fwrite(Command.c_str(), Command.size(), 1, Border->Handle);
+        fflush(Border->Handle);
+    }
+}
+
+inline void
+RefreshBorder(kwm_border *Border, int WindowID)
+{
+    std::string Command = std::to_string(WindowID) + \
+                          " " + Border->Color.Format + \
+                          " s:" + std::to_string(Border->Width);
+
+    Command += Border->Radius != -1 ? " rad:" + std::to_string(Border->Radius) : "";
+
+    fwrite(Command.c_str(), Command.size(), 1, Border->Handle);
+    fflush(Border->Handle);
+}
 
 void UpdateBorder(std::string BorderType);
 
