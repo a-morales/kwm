8fe158bbb526e23f3252c742e176dd74343b9550
koekeishiya <aasvi93@hotmail.com>
2016-03-25 22:58:05 +0100

    update function prototypes

--- kwm/interpreter.h
+++ kwm/interpreter.h
@@ -6,10 +6,19 @@
 void KwmInterpretCommand(std::string Message, int ClientSockFD);
 void KwmConfigCommand(std::vector<std::string> &Tokens);
 void KwmReadCommand(std::vector<std::string> &Tokens, int ClientSockFD);
-void KwmWindowCommand(std::vector<std::string> &Tokens);
+void KwmMarkCommand(std::vector<std::string> &Tokens);
 void KwmTreeCommand(std::vector<std::string> &Tokens);
-void KwmScreenCommand(std::vector<std::string> &Tokens);
-void KwmSpaceCommand(std::vector<std::string> &Tokens);
+void KwmFocusCommand(std::vector<std::string> &Tokens);
+void KwmSwapCommand(std::vector<std::string> &Tokens);
+void KwmZoomCommand(std::vector<std::string> &Tokens);
+void KwmFloatCommand(std::vector<std::string> &Tokens);
+void KwmRefreshCommand(std::vector<std::string> &Tokens);
+void KwmNodeCommand(std::vector<std::string> &Tokens);
+void KwmMoveCommand(std::vector<std::string> &Tokens);
+void KwmMoveCommand(std::vector<std::string> &Tokens);
+void KwmSplitCommand(std::vector<std::string> &Tokens);
+void KwmPaddingCommand(std::vector<std::string> &Tokens);
+void KwmGapCommand(std::vector<std::string> &Tokens);
 void KwmBindCommand(std::vector<std::string> &Tokens);
 
 #endif
