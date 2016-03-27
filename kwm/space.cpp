f7c886b8818fa3fd369cc686b2da8318510e3692
Enrico Ghirardi <i@choco.me>
2016-03-27 22:30:43 +0200

    Destroy notifications for unmanaged spaces

--- kwm/space.cpp
+++ kwm/space.cpp
@@ -322,6 +322,7 @@ void UpdateActiveSpace()
     {
         ClearFocusedWindow();
         ClearMarkedWindow();
+        DestroyApplicationNotifications();
     }
 
     KWMScreen.Transitioning = false;
