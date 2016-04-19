0e337fcf26bdb6aea74554fbb3e172505826dbc0
koekeishiya <aasvi93@hotmail.com>
2016-04-20 01:35:34 +0200

    change mutex_lock to try_lcok

--- kwm/window.cpp
+++ kwm/window.cpp
@@ -788,7 +788,7 @@ void RemoveWindowFromMonocleTree(screen_info *Screen, int WindowID, bool Center,
 
 void AddWindowToTreeOfUnfocusedMonitor(screen_info *Screen, window_info *Window, bool UpdateFocus)
 {
-    pthread_mutex_lock(&KWMThread.Lock);
+    bool Locked = pthread_mutex_trylock(&KWMThread.Lock);
     screen_info *ScreenOfWindow = GetDisplayOfWindow(Window);
     if(!Screen || !Window || Screen == ScreenOfWindow)
         return;
@@ -846,7 +846,9 @@ void AddWindowToTreeOfUnfocusedMonitor(screen_info *Screen, window_info *Window,
         SetWindowFocus(Window);
         MoveCursorToCenterOfFocusedWindow();
     }
-    pthread_mutex_unlock(&KWMThread.Lock);
+
+    if(Locked)
+        pthread_mutex_unlock(&KWMThread.Lock);
 }
 
 void ToggleWindowFloating(int WindowID, bool Center)
