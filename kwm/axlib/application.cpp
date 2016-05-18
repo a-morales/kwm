60926119d30f22bec16b5da4293930f6218f3759
koekeishiya <aasvi93@hotmail.com>
2016-05-19 01:14:57 +0200

    started implementing ax_application

--- kwm/axlib/application.cpp
+++ kwm/axlib/application.cpp
@@ -1,8 +1,45 @@
 #include "application.h"
 
-ax_application AXLibConstructApplication()
+ax_application AXLibConstructApplication(int PID)
 {
-    // TODO(koekeishiya): NYI
     ax_application Application = {};
+
+    Application.Ref = AXUIElementCreateApplication(PID);
+    GetProcessForPID(PID, &Application.PSN);
+    Application.PID = PID;
+
     return Application;
+}
+
+void AXLibAddApplicationWindows(ax_application *Application)
+{
+    CFArrayRef Windows = (CFArrayRef) AXLibGetWindowProperty(Application->Ref,
+                                                             kAXWindowsAttribute);
+
+    if(Windows)
+    {
+        CFIndex Count = CFArrayGetCount(Windows);
+        for(CFIndex Index = 0; Index < Count; ++Index)
+        {
+            AXUIElementRef Ref = (AXUIElementRef) CFArrayGetValueAtIndex(Windows, Index);
+            Application->Windows.add(AXLibConstructWindow(Ref));
+        }
+    }
+
+    return false;
+}
+
+void AXLibRemoveApplicationWindows(ax_application *Application)
+{
+    for(int Index = 0; Index < Application->Windows.size(); ++Index)
+        AXLibDestroyWindow(Application->Windows[Index]);
+
+    Application.Windows.clear();
+}
+
+void AXLibDestroyApplication(ax_application *Application)
+{
+    CFRelease(Application->Ref);
+    Application->Ref = NULL;
+    AXLibRemoveApplicationWindows(Application);
 }
--- kwm/axlib/application.h
+++ kwm/axlib/application.h
@@ -2,16 +2,23 @@
 #define AXLIB_APPLICATION_H
 
 #include "window.h"
+#include <vector>
 
 struct ax_application
 {
     AXUIElementRef Ref;
     int PID;
 
-    // std::vector<ax_window> Windows;
+    ProcessSerialNumber PSN;
+
+    std::vector<ax_window> Windows;
     ax_window *Focus;
 };
 
 ax_application AXLibConstructApplication();
+void AXLibDestroyApplication(ax_application *Application);
+
+void AXLibAddApplicationWindows(ax_application *Application);
+void AXLibRemoveApplicationWindows(ax_application *Application)
 
 #endif
