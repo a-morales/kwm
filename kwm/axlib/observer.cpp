41c5c3d4cbca2c307f341356610bb0b1122a2d76
koekeishiya <aasvi93@hotmail.com>
2016-05-18 19:36:10 +0200

    axlib observers

---
+++ kwm/axlib/observer.cpp
@@ -0,0 +1,61 @@
+#include "observer.h"
+
+/* NOTE(koekeishiya): For compatibility with current Kwm code
+ * TODO: Remove.
+ */
+ax_observer AXLibConstructObserver(int PID, ObserverCallback Callback)
+{
+    ax_observer Observer = {};
+    AXObserverCreate(PID, Callback, &Observer.Ref);
+    Observer.AppRef = AXUIElementCreateApplication(PID);
+    return Observer;
+}
+
+void AXLibAddObserverNotification(ax_observer *Observer, CFStringRef Notification)
+{
+    AXObserverAddNotification(Observer->Ref, Observer->AppRef, Notification, NULL);
+}
+
+void AXLibRemoveObserverNotification(ax_observer *Observer, CFStringRef Notification)
+{
+    AXObserverRemoveNotification(Observer->Ref, Observer->AppRef, Notification);
+}
+
+/* END */
+
+ax_observer AXLibConstructObserver(ax_application *Application, ObserverCallback Callback)
+{
+    ax_observer Observer = {};
+    AXObserverCreate(Application->PID, Callback, &Observer.Ref);
+    Observer.Application = Application;
+    return Observer;
+}
+
+void AXLibStartObserver(ax_observer *Observer)
+{
+    CFRunLoopAddSource(CFRunLoopGetMain(), AXObserverGetRunLoopSource(Observer->Ref), kCFRunLoopDefaultMode);
+}
+
+/* NOTE(koekeishiya): These functions will replace the temporary code above
+void AXLibAddObserverNotification(ax_observer *Observer, CFStringRef Notification)
+{
+    AXObserverAddNotification(Observer->Ref, Observer->Application->Ref, Notification, NULL);
+}
+
+void AXLibRemoveObserverNotification(ax_observer *Observer, CFStringRef Notification)
+{
+    AXObserverRemoveNotification(Observer->Ref, Observer->Application->Ref, Notification);
+}
+*/
+
+void AXLibStopObserver(ax_observer *Observer)
+{
+    CFRunLoopRemoveSource(CFRunLoopGetMain(), AXObserverGetRunLoopSource(Observer->Ref), kCFRunLoopDefaultMode);
+}
+
+void AXLibDestroyObserver(ax_observer *Observer)
+{
+    CFRelease(Observer->Ref);
+    Observer->Ref = NULL;
+    Observer->Application = NULL;
+}
---
+++ kwm/axlib/observer.h
@@ -0,0 +1,30 @@
+#ifndef AXLIB_OBSERVER_H
+#define AXLIB_OBSERVER_H
+
+#include "application.h"
+
+#define OBSERVER_CALLBACK(name) void name(AXObserverRef Observer,\
+                                          AXUIElementRef Element,\
+                                          CFStringRef Notification, void *)
+typedef OBSERVER_CALLBACK(ObserverCallback);
+
+struct ax_observer
+{
+    AXObserverRef Ref;
+    ax_application *Application;
+
+    // Note(koekeishiya): Remove after implementing ax_application
+    AXUIElementRef AppRef;
+};
+
+ax_observer AXLibConstructObserver(int PID, ObserverCallback Callback);
+ax_observer AXLibConstructObserver(ax_application *Application, ObserverCallback Callback);
+void AXLibDestroyObserver(ax_observer *Observer);
+
+void AXLibStartObserver(ax_observer *Observer);
+void AXLibStopObserver(ax_observer *Observer);
+
+void AXLibAddObserverNotification(ax_observer *Observer, CFStringRef Notification);
+void AXLibRemoveObserverNotification(ax_observer *Observer, CFStringRef Notification);
+
+#endif
