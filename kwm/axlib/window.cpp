bc5843b382709317633a64e1ad490d61da7af7c7
koekeishiya <aasvi93@hotmail.com>
2016-05-20 15:28:20 +0200

    find window size and position upon ax_window creation

--- kwm/axlib/window.cpp
+++ kwm/axlib/window.cpp
@@ -1,16 +1,23 @@
 #include "window.h"
 #include "element.h"
 
+#define internal static
+#define local_persist static
+
 ax_window AXLibConstructWindow(ax_application *Application, AXUIElementRef WindowRef)
 {
     ax_window Window = {};
 
     Window.Ref = WindowRef;
     Window.Application = Application;
-    Window.Name = AXLibGetWindowTitle(WindowRef);
     Window.ID = AXLibGetWindowID(WindowRef);
+    Window.Name = AXLibGetWindowTitle(WindowRef);
+
+    Window.Position = AXLibGetWindowPosition(WindowRef);
+    Window.Size = AXLibGetWindowSize(WindowRef);
     Window.Movable = AXLibIsWindowMovable(WindowRef);
     Window.Resizable = AXLibIsWindowResizable(WindowRef);
+
     AXLibGetWindowRole(WindowRef, &Window.Type.Role);
     AXLibGetWindowSubrole(WindowRef, &Window.Type.Subrole);
 
