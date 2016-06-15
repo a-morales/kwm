3e8acf0884d8ce7d81f8507b2b3a299bd2da4df9
koekeishiya <aasvi93@hotmail.com>
2016-06-16 01:42:27 +0200

    delete old code

--- kwm/space.cpp
+++ kwm/space.cpp
@@ -102,212 +102,31 @@ void GoToPreviousSpace(bool MoveFocusedWindow)
     */
 }
 
-bool IsActiveSpaceFloating()
-{
-    return IsSpaceFloating(KWMScreen.Current->ActiveSpace);
-}
+bool IsActiveSpaceFloating() { }
 
-bool IsSpaceFloating(int SpaceID)
-{
-    bool Result = false;
-
-    if(IsSpaceInitializedForScreen(KWMScreen.Current))
-    {
-        std::map<int, space_info>::iterator It = KWMScreen.Current->Space.find(SpaceID);
-        if(It != KWMScreen.Current->Space.end())
-            Result = KWMScreen.Current->Space[SpaceID].Settings.Mode == SpaceModeFloating;
-    }
-
-    return Result;
-}
+bool IsSpaceFloating(int SpaceID) { }
 
 space_info *GetActiveSpaceOfScreen(screen_info *Screen) { }
 
-bool IsSpaceInitializedForScreen(screen_info *Screen)
-{
-    if(!Screen)
-        return false;
+bool IsSpaceInitializedForScreen(screen_info *Screen) { }
 
-    std::map<int, space_info>::iterator It = Screen->Space.find(Screen->ActiveSpace);
-    if(It == Screen->Space.end())
-        return false;
-    else
-        return It->second.Initialized;
-}
+bool DoesSpaceExistInMapOfScreen(screen_info *Screen) { }
 
-bool DoesSpaceExistInMapOfScreen(screen_info *Screen)
-{
-    if(!Screen)
-        return false;
+bool IsSpaceTransitionInProgress() { }
 
-    std::map<int, space_info>::iterator It = Screen->Space.find(Screen->ActiveSpace);
-    if(It == Screen->Space.end())
-        return false;
-    else
-        return It->second.RootNode != NULL && It->second.Initialized;
-}
+bool IsActiveSpaceManaged() { }
 
-bool IsSpaceTransitionInProgress()
-{
-    bool Result = false;
-    std::map<unsigned int, screen_info>::iterator It;
-    for(It = KWMTiling.DisplayMap.begin(); It != KWMTiling.DisplayMap.end(); ++It)
-    {
-        screen_info *Screen = &It->second;
-        Result = Result || CGSManagedDisplayIsAnimating(CGSDefaultConnection, Screen->Identifier);
-    }
+void ShouldActiveSpaceBeManaged() { }
 
-    if(Result)
-    {
-        DEBUG("IsSpaceTransitionInProgress() Space transition detected");
-        if(!KWMMach.DisableEventTapInternal)
-        {
-            KWMMach.DisableEventTapInternal = true;
-            CGEventTapEnable(KWMMach.EventTap, false);
-            KWMScreen.Transitioning = true;
-        }
-    }
-    else if(KWMMach.DisableEventTapInternal)
-    {
-        KWMMach.DisableEventTapInternal = false;
-        CGEventTapEnable(KWMMach.EventTap, true);
-    }
+void FloatFocusedSpace() { }
 
-    return Result;
-}
+void UpdateActiveSpace() { }
 
-bool IsActiveSpaceManaged()
-{
-    space_info *Space = GetActiveSpaceOfScreen(KWMScreen.Current);
-    return Space->Managed;
-}
+void SetNameOfActiveSpace(screen_info *Screen, std::string Name) {}
 
-void ShouldActiveSpaceBeManaged()
-{
-    space_info *Space = GetActiveSpaceOfScreen(KWMScreen.Current);
-    Space->Managed = CGSSpaceGetType(CGSDefaultConnection, KWMScreen.Current->ActiveSpace) == CGSSpaceTypeUser;
-}
+std::string GetNameOfSpace(screen_info *Screen, int CGSpaceID) { }
 
-void FloatFocusedSpace()
-{
-    if(KWMToggles.EnableTilingMode &&
-       !IsSpaceTransitionInProgress() &&
-       IsActiveSpaceManaged())
-    {
-        space_info *Space = GetActiveSpaceOfScreen(KWMScreen.Current);
-        if(Space->Settings.Mode == SpaceModeFloating)
-            return;
-
-        DestroyNodeTree(Space->RootNode);
-        Space->RootNode = NULL;
-
-        Space->Settings.Mode = SpaceModeFloating;
-        Space->Initialized = true;
-        Space->NeedsUpdate = false;
-        ClearFocusedWindow();
-    }
-}
-
-void TileFocusedSpace(space_tiling_option Mode)
-{
-    if(KWMToggles.EnableTilingMode &&
-       !IsSpaceTransitionInProgress() &&
-       IsActiveSpaceManaged() &&
-       FilterWindowList(KWMScreen.Current))
-    {
-        space_info *Space = GetActiveSpaceOfScreen(KWMScreen.Current);
-        if(Space->Settings.Mode == Mode)
-            return;
-
-        DestroyNodeTree(Space->RootNode);
-        Space->RootNode = NULL;
-
-        Space->Settings.Mode = Mode;
-        std::vector<window_info*> WindowsOnDisplay = GetAllWindowsOnDisplay(KWMScreen.Current->ID);
-        CreateWindowNodeTree(KWMScreen.Current, &WindowsOnDisplay);
-    }
-}
-
-void UpdateActiveSpace()
-{
-    /*
-    ClearFocusedWindow();
-    ClearMarkedWindow();
-
-    Assert(KWMScreen.Current);
-
-    if(KWMTiling.KwmOverlay[0] != 0)
-        RemoveWindowFromSpace(KWMScreen.Current->ActiveSpace, KWMTiling.KwmOverlay[0]);
-    if(KWMTiling.KwmOverlay[1] != 0)
-        RemoveWindowFromSpace(KWMScreen.Current->ActiveSpace, KWMTiling.KwmOverlay[1]);
-
-    KWMScreen.PrevSpace = KWMScreen.Current->ActiveSpace;
-    KWMScreen.Current->ActiveSpace = GetActiveSpaceOfDisplay(KWMScreen.Current);
-    ShouldActiveSpaceBeManaged();
-    AXLibRunningApplications();
-
-    space_info *Space = NULL;
-    if(KWMScreen.PrevSpace != KWMScreen.Current->ActiveSpace)
-    {
-        DEBUG("UpdateActiveSpace() Space transition ended " << KWMScreen.PrevSpace << " -> " << KWMScreen.Current->ActiveSpace);
-        if(KWMScreen.Current->TrackSpaceChange)
-            KWMScreen.Current->History.push(KWMScreen.PrevSpace);
-        else
-            KWMScreen.Current->TrackSpaceChange = true;
-
-        Space = GetActiveSpaceOfScreen(KWMScreen.Current);
-        UpdateActiveWindowList(KWMScreen.Current);
-
-        if(Space->NeedsUpdate)
-            UpdateSpaceOfScreen(Space, KWMScreen.Current);
-
-        if(KWMScreen.Current->RestoreFocus &&
-           Space->FocusedWindowID != -1)
-        {
-            FocusWindowByID(Space->FocusedWindowID);
-            MoveCursorToCenterOfFocusedWindow();
-        }
-        else
-        {
-            FocusWindowOfOSX();
-            MoveCursorToCenterOfFocusedWindow();
-            KWMScreen.Current->RestoreFocus = true;
-        }
-    }
-    else
-    {
-        std::map<unsigned int, screen_info>::iterator It;
-        for(It = KWMTiling.DisplayMap.begin(); It != KWMTiling.DisplayMap.end(); ++It)
-        {
-            screen_info *Screen = &It->second;
-            if(Screen->ID == KWMScreen.Current->ID)
-                continue;
-
-            int ScreenCurrentSpace = Screen->ActiveSpace;
-            int ScreenNewSpace = GetActiveSpaceOfDisplay(Screen);
-            if(ScreenCurrentSpace != ScreenNewSpace)
-            {
-                DEBUG("space changed on monitor: " << Screen->ID);
-
-                Screen->History.push(Screen->ActiveSpace);
-                Screen->ActiveSpace = ScreenNewSpace;
-                KWMScreen.PrevSpace = KWMScreen.Current->ActiveSpace;
-                KWMScreen.Current = Screen;
-                ShouldActiveSpaceBeManaged();
-                Space = GetActiveSpaceOfScreen(Screen);
-                UpdateActiveWindowList(Screen);
-                FilterWindowList(Screen);
-                break;
-            }
-        }
-    }
-
-    if(KWMTiling.KwmOverlay[0] != 0)
-        AddWindowToSpace(KWMScreen.Current->ActiveSpace, KWMTiling.KwmOverlay[0]);
-    if(KWMTiling.KwmOverlay[1] != 0)
-        AddWindowToSpace(KWMScreen.Current->ActiveSpace, KWMTiling.KwmOverlay[1]);
-    */
-}
+void TileFocusedSpace(space_tiling_option Mode) { }
 
 space_settings *GetSpaceSettingsForDesktopID(int ScreenID, int DesktopID)
 {
@@ -333,15 +152,11 @@ int GetSpaceFromName(ax_display *Display, std::string Name)
     return -1;
 }
 
-void SetNameOfActiveSpace(screen_info *Screen, std::string Name) {}
-
 void SetNameOfActiveSpace(ax_display *Display, std::string Name)
 {
     space_info *SpaceInfo = &WindowTree[Display->Space->Identifier];
     if(SpaceInfo) SpaceInfo->Settings.Name = Name;
 }
-
-std::string GetNameOfSpace(screen_info *Screen, int CGSpaceID) { }
 
 std::string GetNameOfSpace(ax_display *Display)
 {
